80d9958ef31dadc918b6fb8446818aef13b2578b
Matt Heffernan <slithymatt@hotmail.com>
2021-08-18 23:46:47 -0400

    C64 using CHROUT, 26.75 seconds

---
+++ c64/c64-mandelbrot_chrout.asm
@@ -0,0 +1,64 @@
+.org $080D
+.segment "STARTUP"
+.segment "INIT"
+.segment "ONCE"
+.segment "CODE"
+
+   jmp start
+
+.include "../6502/mandelbrot.asm"
+
+RETURN            = $0D
+REVERSE_ON        = $12
+SPACE             = $20
+REVERSE_OFF       = $92
+CLEAR_SCREEN      = $93
+
+SCREEN_MAP        = $0400
+COLOR_MAP         = $D800
+
+CHROUT            = $FFD2
+PLOT              = $FFF0
+
+i_result: .byte 0
+
+color_codes:
+   .byte $05, $1C, $1E, $1F   ; white, red, green, blue
+   .byte $81, $96, $95, $97   ; orange, lt red, brown, dk gray
+   .byte $98, $99, $9A, $9B   ; md gray, lt green, lt blue, lt gray
+   .byte $9C, $9E, $90        ; purple, yellow, black
+
+start:
+   lda #CLEAR_SCREEN
+   jsr CHROUT
+   lda #REVERSE_ON
+   jsr CHROUT
+   ldx #0
+   ldy #0
+@loop:
+   lda mand_max_it
+   jsr mand_get
+   sta i_result
+   txa
+   pha ; preserve X
+   ldx i_result
+   lda color_codes,x
+   jsr CHROUT
+   lda #SPACE
+   jsr CHROUT
+   pla
+   tax ; restore X
+   inx
+   cpx mand_width
+   bne @loop
+   lda #RETURN
+   jsr CHROUT
+   lda #REVERSE_ON
+   jsr CHROUT
+   ldx #0
+   iny
+   cpy mand_height
+   bne @loop
+   lda #$9A ; restore lt blue text
+   jsr CHROUT
+   rts
