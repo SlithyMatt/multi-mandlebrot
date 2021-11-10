e22c88093bc989a1541cbc455776847ab3c3bd02
Matt Heffernan <slithymatt@hotmail.com>
2021-11-09 23:56:46 -0500

    removed temporary build target

--- mega65/build.sh
+++ mega65/build.sh
@@ -3,7 +3,7 @@
 acme -r man.list mega65-mandelbrot.asm
 
 ## create disk image with man.prg
-#cbmconvert -D8o MAN65.D81 -n man.prg
+cbmconvert -D8o MAN65.D81 -n man.prg
 
 # add man.prg to test disk
-c1541 -attach ~/nextCloud/stuff/Hardware/mega65/disks/M65MAN.D81 -delete man65asm -write man.prg man65asm
+#c1541 -attach ~/nextCloud/stuff/Hardware/mega65/disks/M65MAN.D81 -delete man65asm -write man.prg man65asm
