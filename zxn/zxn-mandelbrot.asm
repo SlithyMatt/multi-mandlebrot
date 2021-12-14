0de9e4e287b446417fa59eef5a7cb4407b613a24
Matt Heffernan <slithymatt@hotmail.com>
2021-12-14 01:35:59 -0500

    defined ULANext palette

--- zxn/zxn-mandelbrot.asm
+++ zxn/zxn-mandelbrot.asm
@@ -21,10 +21,48 @@ color_codes:
    db 72,80,88,96,104,112,120
 
 init:
-  nextreg $07,$03
+   nextreg $07,$03      ; set to 28 MHz
    exx                  ; save hl' register on stack
 	push hl              ; to correct return into basic
-;;;    call ROM_CLS
+   nextreg $43,$01      ; enable ULAnext, first ULA palette, auto-increment
+   nextreg $42,15       ; 16 color mode
+   nextreg $44,%00000000   ; Color 0: black
+   nextreg $44,%00000000
+   nextreg $44,%00100100   ; Color 1: gray 1
+   nextreg $44,%00000001
+   nextreg $44,%01001001   ; Color 2: gray 2
+   nextreg $44,%00000000
+   nextreg $44,%01101101   ; Color 3: gray 3
+   nextreg $44,%00000001
+   nextreg $44,%10010010   ; Color 4: gray 4
+   nextreg $44,%00000000
+   nextreg $44,%10110110   ; Color 5: gray 5
+   nextreg $44,%00000001
+   nextreg $44,%10010010   ; Color 4: gray 4
+   nextreg $44,%00000000
+   nextreg $44,%10110110   ; Color 5: gray 5
+   nextreg $44,%00000001
+   nextreg $44,%11011011   ; Color 6: gray 6
+   nextreg $44,%00000000
+   nextreg $44,%11111111   ; Color 7: white
+   nextreg $44,%00000001
+   nextreg $44,%00100100   ; Color 8: yellow 1
+   nextreg $44,%00000000
+   nextreg $44,%01001000   ; Color 9: yellow 2
+   nextreg $44,%00000000
+   nextreg $44,%01101100   ; Color 10: yellow 3
+   nextreg $44,%00000000
+   nextreg $44,%10010000   ; Color 11: yellow 4
+   nextreg $44,%00000000
+   nextreg $44,%10110100   ; Color 12: yellow 5
+   nextreg $44,%00000000
+   nextreg $44,%11011000   ; Color 13: yellow 6
+   nextreg $44,%00000000
+   nextreg $44,%11111100   ; Color 14: yellow 7
+   nextreg $44,%00000000
+   nextreg $44,%00100000   ; Color 15: red 1
+   nextreg $44,%00000000
+
    ld bc,0              ; X = 0, Y = 0
 .loopm:
    call mand_get
