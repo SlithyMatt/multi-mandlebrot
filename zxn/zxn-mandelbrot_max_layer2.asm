8dc51652d8c33481f9848c0c812b01bba41de9a5
Matt Heffernan <slithymatt@hotmail.com>
2022-01-09 01:03:51 -0500

    ZXN Layer 2 working at 256x192

--- zxn/zxn-mandelbrot_max_layer2.asm
+++ zxn/zxn-mandelbrot_max_layer2.asm
@@ -7,7 +7,7 @@ start:
 
 MAND_WIDTH  = 256
 MAND_HEIGHT = 192
-MAND_MAX_IT = 22
+MAND_MAX_IT = 48
 
 	include "../Z80n/mandelbrot24.asm"
 
@@ -20,60 +20,20 @@ next_bank: db 18
 
 init:
    nextreg $07,$03         ; set to 28 MHz
-   nextreg $70,$00;10         ; Layer 2: 320x256x8bpp
+   nextreg $70,$00         ; Layer 2: 256x192x8bpp
    ld bc,$123B             ; Layer 2 Access Port
    ld a,$02                ; Display Layer 2
+   out (c),a
    nextreg $15,$0C         ; place Layer 2 on top
    nextreg $6A,$00         ; 256-color
-   nextreg $52,16          ; Set 16k slot 4 to bank 8 (8k slot 2 = 8k bank 16)
-   nextreg $53,17          ; (8k slot 7 = 8k bank 17)
-   nextreg $42,$FF         ; Palette all-ink
-   nextreg $43,$00         ; select ULA palette 1, no ULANext
-   nextreg $44,%00000000   ; Color 0: black
-   nextreg $44,%00000000
-   nextreg $44,%00100100   ; Color 1: gray 1
-   nextreg $44,%00000001
-   nextreg $44,%01001001   ; Color 2: gray 2
-   nextreg $44,%00000000
-   nextreg $44,%01101101   ; Color 3: gray 3
-   nextreg $44,%00000001
-   nextreg $44,%10010010   ; Color 4: gray 4
-   nextreg $44,%00000000
-   nextreg $44,%10110110   ; Color 5: gray 5
-   nextreg $44,%00000001
-   nextreg $44,%11011011   ; Color 6: gray 6
-   nextreg $44,%00000000
-   nextreg $44,%11111111   ; Color 7: white
-   nextreg $44,%00000001
-   nextreg $44,%00100000   ; Color 8: red 1
-   nextreg $44,%00000000
-   nextreg $44,%01000000   ; Color 9: red 2
-   nextreg $44,%00000000
-   nextreg $44,%01100000   ; Color 10: red 3
-   nextreg $44,%00000000
-   nextreg $44,%10000000   ; Color 11: red 4
-   nextreg $44,%00000000
-   nextreg $44,%10100000   ; Color 12: red 5
-   nextreg $44,%00000000
-   nextreg $44,%11000000   ; Color 13: red 6
-   nextreg $44,%00000000
-   nextreg $44,%11100000   ; Color 14: red 7
-   nextreg $44,%00000000
-   nextreg $44,%00100100   ; Color 15: yellow 1
-   nextreg $44,%00000000
-   nextreg $44,%01001000   ; Color 16: yellow 2
-   nextreg $44,%00000000
-   nextreg $44,%01101100   ; Color 17: yellow 3
-   nextreg $44,%00000000
-   nextreg $44,%10010000   ; Color 18: yellow 4
-   nextreg $44,%00000000
-   nextreg $44,%10110100   ; Color 19: yellow 5
-   nextreg $44,%00000000
-   nextreg $44,%11011000   ; Color 20: yellow 6
-   nextreg $44,%00000000
-   nextreg $44,%11111100   ; Color 21: yellow 7
-   nextreg $44,%00000000
-
+   nextreg $52,16          ; Set 16k slot 2 to bank 8 (8k slot 2 = 8k bank 16)
+   nextreg $53,17          ; (8k slot 3 = 8k bank 17)
+   nextreg $12,8           ; Start Layer 2 RAM at bank 8
+   nextreg $1C,1           ; Set Layer 2 Clip Window
+   nextreg $18,0           ; X1 = 0
+   nextreg $18,255         ; X2 = 255
+   nextreg $18,0           ; Y1 = 0
+   nextreg $18,191         ; Y2 = 191
    ld bc,0              ; X = 0, Y = 0
 .loopm:
    call mand_get
@@ -92,24 +52,24 @@ init:
    cp h
    jp nz,.savenext
    ld a,(next_bank)
-   nextreg $56,a
+   nextreg $52,a
    inc a
-   nextreg $57,a
+   nextreg $53,a
    inc a
    ld (next_bank),a
    ld hl,LAYER2_START
 .savenext
    ld (next_pixel),hl
-   inc c                ; increment Y
-   ld a,MAND_HEIGHT
-   cp c
-   jp nz,.loopm         ; loop until Y = height
-   ld c,0               ; Y = 0
-   inc hl
    inc b                ; increment X
    ld a,low MAND_WIDTH
    cp b
    jp nz,.loopm         ; loop until X = width
+   ld b,0               ; X = 0
+   inc hl
+   inc c                ; increment Y
+   ld a,MAND_HEIGHT
+   cp c
+   jp nz,.loopm         ; loop until Y = height
 .loop_end:
 	jp .loop_end
    ret
