192f160565d2401173601ded25b30e470f7ff735
Matt Heffernan <matthew.heffernan.3@us.af.mil>
2022-01-11 15:13:06 -0500

    refactored Layer 2 demo for 320x256

--- zxn/zxn-mandelbrot_max_layer2.asm
+++ zxn/zxn-mandelbrot_max_layer2.asm
@@ -5,8 +5,8 @@
 start:
 	jp init
 
-MAND_WIDTH  = 256
-MAND_HEIGHT = 192
+MAND_WIDTH  = 320
+MAND_HEIGHT = 256
 MAND_MAX_IT = 48
 
 	include "../Z80n/mandelbrot24.asm"
@@ -20,7 +20,7 @@ next_bank: db 18
 
 init:
    nextreg $07,$03         ; set to 28 MHz
-   nextreg $70,$00         ; Layer 2: 256x192x8bpp
+   nextreg $70,$10         ; Layer 2: 320x256x8bpp
    ld bc,$123B             ; Layer 2 Access Port
    ld a,$02                ; Display Layer 2
    out (c),a
@@ -31,10 +31,12 @@ init:
    nextreg $12,8           ; Start Layer 2 RAM at bank 8
    nextreg $1C,1           ; Set Layer 2 Clip Window
    nextreg $18,0           ; X1 = 0
-   nextreg $18,255         ; X2 = 255
+   nextreg $18,159         ; X2 = 159
    nextreg $18,0           ; Y1 = 0
-   nextreg $18,191         ; Y2 = 191
-   ld bc,0              ; X = 0, Y = 0
+   nextreg $18,127         ; Y2 = 127
+   ld bc,0                 ; X = 0, Y = 0
+   ld (mand_x),bc
+   ld (mand_y),bc
 .loopm:
    call mand_get
    ld hl,(next_pixel)
@@ -60,16 +62,27 @@ init:
    ld hl,LAYER2_START
 .savenext
    ld (next_pixel),hl
-   inc b                ; increment X
-   ld a,low MAND_WIDTH
+   ld bc,(mand_y)
+   inc bc
+   ld (mand_y),bc       ; increment Y
+   ld a,high MAND_HEIGHT
    cp b
-   jp nz,.loopm         ; loop until X = width
-   ld b,0               ; X = 0
-   inc hl
-   inc c                ; increment Y
-   ld a,MAND_HEIGHT
+   jp nz,.loopm         ; loop until Y = height
+   ld a,low MAND_HEIGHT
    cp c
    jp nz,.loopm         ; loop until Y = height
+   ld bc,0
+   ld (mand_y),bc       ; Y = 0
+   inc hl
+   ld bc,(mand_x)
+   inc bc
+   ld (mand_x),bc       ; increment X
+   ld a,high MAND_WIDTH
+   cp b
+   jp nz,.loopm         ; loop until X = width
+   ld a,low MAND_WIDTH
+   cp c
+   jp nz,.loopm         ; loop until X = width
 .loop_end:
 	jp .loop_end
    ret
