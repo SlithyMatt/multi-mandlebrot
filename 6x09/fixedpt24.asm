ec8576b9cbf7c75e657a3d48593875e794df3af2
Theodore (Alex) Evans <varmfslii@gmail.com>
2022-05-01 23:27:36 -0400

    first pass on completed fp24 (untested)

--- 6x09/fixedpt24.asm
+++ 6x09/fixedpt24.asm
@@ -20,99 +20,109 @@ FP24_LD_WORD macro 		; (u)=(ea) ; int
 	ldu #FP24_ACC
 	endm
 	
-FP24_SUBTRACT macro		; (u)=(x)-(y)
+FP24_SUBTRACT macro		; (x)=(x)-(y)
 	ldd 1,x
 	subd 1,y
-	std 1,u
+	std 1,x
 	lda ,x
 	sbca ,x
-	sta ,u
+	sta ,x
 	endm
 
-FP24_ADD macro			; (u)=(x)+(y)
+FP24_ADD macro			; (x)=(x)+(y)
 	ldd 1,x
 	addd 1,y
-	std 1,u
+	std 1,x
 	lda ,x
 	adca ,y
-	sta ,u
+	sta ,x
 	endm
 
-FP24_NEG macro			; (ea)=-(ea)
-	com \1
-	com \1+1
-	neg \1+2
-	ldd #0
-	adcb \1+1
-	adca \1
-	std \1
+FP24_NEG macro			; (x)=-(x)
+	com ,x
+	ldd 1,x
+	coma
+	comb
+	addd #1
+	std 1,x
+	lda #0
+	adca ,x
+	sta ,x
 	endm
 
-FP24_ABS macro			; acc=|acc|
-	tst \1
+FP24_ABS macro			; (x)=|(x)|
+	tst ,x
 	bpl out@
 	FP24_NEG
 out@:
 	endm
 	
-FP24_MULTIPLY macro		; (u)=(x)*(y)
+cp_abs:
+	lda ,x
+	sta ,y
+	ldd 1,x
+	std 1,y
+	tst ,x
+	bpl out@
+	com ,y
+	coma
+	comb
+	addd #1
+	std 1,y
+	lda #0
+	adca ,y
+	sta ,y
+out@:
+	rts
+
+
+FP24_MULTIPLY macro		; (x)=(x)*(y)
 	leas -8,s
-	stu 6,s
 	leau ,s
 	lbsr muls24_48
-	leau 6,s
 	ldd 2,s
 	std ,x
 	lda 4,s
-	sta 2,u
+	sta 2,x
 	leas 8,s
 	endm
 
-FP24_DIVIDE macro 		; (u)=(x)/(y) 
-	ldx \1
+FP24_DIVIDE macro 		; (x)=(x)/(y) 
 	lbsr fp_div
 	endm
 	
 fp_div: ; d=d/x ; remainder in FP24_XT
-	FP24_ST FP24_A
-	FP24_ABS
-	FP24_ST FP24_AA
-	tfr x,d
-	FP24_ST FP24_B
-	FP24_ABS
-	FP24_ST FP24_BA
-
- 	ldb FP24_BA+1
- 	clra
- 	FP24_ST FP24_BA
- 	clrb
- 	std FP24_XT	
- 	ldx #16     ;There are 16 bits in C
-@loop1:
-	FP24_LD FP24_AA
-	aslb    ;Shift hi bit of C into REM
-	rola  ;(vacating the lo bit, which will be used for the quotient)
-	FP24_ST FP24_AA
-	FP24_LD FP24_XT
-	rolb
-	rola
-	FP24_ST FP24_XT
-	subd FP24_B ;Trial subtraction
-	blt @loop2  ;Did subtraction succeed?
-	std FP24_XT
-	inc FP24_AA+1    ;and record a 1 in the quotient
-@loop2:
- 	leax -1,x	
- 	bne @loop1
-	lda FP24_A
-	eora FP24_B
-	blt @retneg
-	FP24_LD FP24_AA
-	rts
-@retneg:
-	FP24_LD FP24_AA
-	FP24_NEG
-	rts
+	pshs y,x
+	ldy #FP24_A
+	lbsr cp_abs
+	ldx ,s
+	ldy #FP24_B
+	lbsr cp_abs
+	ldx #32
+loop@:
+	asl FP24_A+2
+	rol FP24_A+1
+	rol FP24_A+1
+	rol FP24_R+2
+	rol FP24_R+1
+	rol FP24_R
+	ldd FP24_R+1
+	subd FP24_B+1
+	std FP24_T+1
+	lda FP24_R
+	sbca FP24_B
+	blt skip@
+	sta FP24_R
+	ldd FP24_T+1
+	std FP24_R
+	inc FP24_A+2
+skip@:
+	leax -1,x
+	bne loop@
+	ldy 2,s
+	ldx FP24_A
+	lbsr cp_abs
+	puls x,y,pc
 
 muls24_48:
 	pshs x,y
