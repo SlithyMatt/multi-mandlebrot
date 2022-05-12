c4ff843ef4ac3cc2a6a700d56b1f9a48e5b0262b
Matt Heffernan <slithymatt@hotmail.com>
2022-05-12 13:58:57 -0400

    working with threads

--- modern/mandelbrot.cpp
+++ modern/mandelbrot.cpp
@@ -1,18 +1,50 @@
 #define cimg_use_png 1
 #include "CImg.h"
 #include <array>
+#include <atomic>
 #include <chrono>
 #include <iostream>
+#include <queue>
+#include <thread>
 
 using namespace cimg_library;
 using namespace std;
 
 #define WIDTH 3840
 #define HEIGHT 2160
-#define MAX_ITER 200
+#define MAX_ITER 2000
 #define NUM_THREADS 16
 
-void row(int py, CImg<unsigned char>& img, array<array<unsigned char,3>,MAX_ITER>& palette) {
+#if NUM_THREADS > HEIGHT
+#error "Can't have more threads than pixel rows!"
+#endif
+
+CImg<unsigned char> img(WIDTH,HEIGHT,1,3,0);
+
+unsigned char cga_palette[16][3] = { // CGA 16-color palette, as RGB
+   {0x00,0x00,0x00},
+   {0x00,0x00,0xAA},
+   {0x00,0xAA,0x00},
+   {0x00,0xAA,0xAA},
+   {0xAA,0x00,0x00},
+   {0xAA,0x00,0xAA},
+   {0xAA,0x55,0x00},
+   {0xAA,0xAA,0xAA},
+   {0x55,0x55,0x55},
+   {0x55,0x55,0xFF},
+   {0x55,0xFF,0x55},
+   {0x55,0xFF,0xFF},
+   {0xFF,0x55,0x55},
+   {0xFF,0x55,0xFF},
+   {0xFF,0xFF,0x55},
+   {0xFF,0xFF,0xFF}
+};
+
+array<array<unsigned char,3>,MAX_ITER> palette;
+
+atomic<int> active_threads(0);
+
+void dorow(int py) {
    int px,i;
    double xz,yz,x,y,xt;
 
@@ -36,33 +68,16 @@ void row(int py, CImg<unsigned char>& img, array<array<unsigned char,3>,MAX_ITER
       img(px,py,0,1) = palette[i][1]; // G
       img(px,py,0,2) = palette[i][2]; // B
    }
+
+   active_threads--;
 }
 
 int main() {
    auto start = chrono::system_clock::now();
-   CImg<unsigned char> img(WIDTH,HEIGHT,1,3,0);
 
-   unsigned char cga_palette[16][3] = { // CGA 16-color palette, as RGB
-      {0x00,0x00,0x00},
-      {0x00,0x00,0xAA},
-      {0x00,0xAA,0x00},
-      {0x00,0xAA,0xAA},
-      {0xAA,0x00,0x00},
-      {0xAA,0x00,0xAA},
-      {0xAA,0x55,0x00},
-      {0xAA,0xAA,0xAA},
-      {0x55,0x55,0x55},
-      {0x55,0x55,0xFF},
-      {0x55,0xFF,0x55},
-      {0x55,0xFF,0xFF},
-      {0xFF,0x55,0x55},
-      {0xFF,0x55,0xFF},
-      {0xFF,0xFF,0x55},
-      {0xFF,0xFF,0xFF}
-   };
-
-   array<array<unsigned char,3>,MAX_ITER> palette;
    int py,i;
+   array<thread *,NUM_THREADS> rowthreads;
+   queue<int> rowq;
 
    for (i=0; i<MAX_ITER; i++) {
       if (i<16) {
@@ -80,9 +95,32 @@ int main() {
       }
    }
 
-   for (py=0; py<HEIGHT; py++) {
-      row(py,img,palette);
+   for (i=0; i<NUM_THREADS; i++) {
+      rowthreads[i] = NULL;
    }
+
+   for (py=0; py<HEIGHT; py++) {
+      rowq.push(py);
+   }
+
+   while (rowq.size() > 0) {
+      for (i=0; i<NUM_THREADS; i++) {
+         if ((rowthreads[i] == NULL) || rowthreads[i]->joinable()) {
+            if (rowthreads[i] != NULL) {
+               rowthreads[i]->join();
+               delete rowthreads[i];
+            }
+            if (rowq.size() > 0) {
+               py = rowq.front();
+               rowq.pop();
+               active_threads++;
+               rowthreads[i] = new thread(dorow,py);
+            }
+         }
+      }
+   }
+
+   while (active_threads > 0);
 
    img.save_png("mandelbrot.png");
 
