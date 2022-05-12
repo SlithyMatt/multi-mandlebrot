f0b3dfa854f7a19e3b12abea4e171393223b8dff
Matt Heffernan <slithymatt@hotmail.com>
2022-05-12 01:13:52 -0400

    modern plot can have any resolution/depth at compile time

--- modern/mandelbrot.cpp
+++ modern/mandelbrot.cpp
@@ -7,14 +7,15 @@
 using namespace cimg_library;
 using namespace std;
 
-#define WIDTH 1920
-#define HEIGHT 1080
+#define WIDTH 3840
+#define HEIGHT 2160
+#define MAX_INT 2000
 
 int main() {
    auto start = chrono::system_clock::now();
    CImg<unsigned char> img(WIDTH,HEIGHT,1,3,0);
 
-   unsigned char palette[16][3] = { // CGA 16-color palette, as RGB
+   unsigned char cga_palette[16][3] = { // CGA 16-color palette, as RGB
       {0x00,0x00,0x00},
       {0x00,0x00,0xAA},
       {0x00,0xAA,0x00},
@@ -33,8 +34,25 @@ int main() {
       {0xFF,0xFF,0xFF}
    };
 
+   array<array<unsigned char,3>,MAX_INT> palette;
    int px,py,i;
    double xz,yz,x,y,xt;
+
+   for (i=0; i<MAX_INT; i++) {
+      if (i<16) {
+         palette[i][0] = cga_palette[i][0];
+         palette[i][1] = cga_palette[i][1];
+         palette[i][2] = cga_palette[i][2];
+      } else if (i % 16 == 0) {
+         palette[i][0] = i & 0x10 ? 255 : 0;
+         palette[i][1] = i & 0x20 ? 255 : 0;
+         palette[i][2] = i & 0x40 ? 255 : 0;
+      } else {
+         palette[i][0] = palette[i-1][0] > 8 ? palette[i-1][0] - 8 : 0;
+         palette[i][1] = palette[i-1][1] > 8 ? palette[i-1][1] - 8 : 0;
+         palette[i][2] = palette[i-1][2] > 8 ? palette[i-1][2] - 8 : 0;
+      }
+   }
 
    for (py=0; py<HEIGHT; py++) {
       for (px=0; px<WIDTH; px++) {
@@ -42,7 +60,7 @@ int main() {
          yz = (double)py*2.0/HEIGHT-1.0;
          x = 0.0;
          y = 0.0;
-         for (i=0; i<15; i++) {
+         for (i=0; i<MAX_INT; i++) {
             if (x*x+y*y > 4) {
                break;
             }
@@ -50,7 +68,7 @@ int main() {
             y = 2*x*y + yz;
             x = xt;
          }
-         if (i > 14) {
+         if (i >= MAX_INT) {
             i = 0;
          }
          img(px,py,0,0) = palette[i][0]; // R
