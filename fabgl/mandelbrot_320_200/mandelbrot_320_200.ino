4b11a7fcdff3c3d99eb19f18519bceb12ad08d54
SlithyMatt <matt@slithygames.com>
2025-05-31 12:06:14 -0400

    random leftovers

--- fabgl/mandelbrot_320_200/mandelbrot_320_200.ino
+++ fabgl/mandelbrot_320_200/mandelbrot_320_200.ino
@@ -1,34 +1,28 @@
 #include "fabgl.h"
 
 fabgl::VGAController dc;
-fabgl::Canvas        canvas(&displayController);
+fabgl::Canvas        canvas(&dc);
 fabgl::PS2Controller ps2c;
+bool written = false;
 
-class PlotScene : public Scene {
-  PlotScene() : Scene(0,20,dc.getViewPortWidth(),dc.getViewPortHeight()) {    
-  }
-
-  if (ps2controller.keyboard()->isVKDown(fabgl::VK_RETURN)) {
-    canvas.clear();
-    written = false;
-  } else if (!written) {
-    canvas.setBrushColor(0,0,2);
-    canvas.clear();
-    canvas.selectFont(&fabgl::FONT_8x8);
-    canvas.setPenColor(2,2,2);
-    canvas.drawText(100,80,"Hit ENTER when ready...");
-    written = true;
-  }
-}
 
 void setup() {
-  ps2controller.begin(PS2Preset::KeyboardPort0_MousePort1, KbdMode::GenerateVirtualKeys);
+  ps2c.begin(PS2Preset::KeyboardPort0_MousePort1, KbdMode::GenerateVirtualKeys);
 
-  displayController.begin();
-  displayController.setResolution(VGA_320x200_75Hz);
+  dc.begin();
+  dc.setResolution(VGA_320x200_75Hz);
+  canvas.setBrushColor(0,0,2);
+  canvas.selectFont(&fabgl::FONT_8x8);
+  canvas.setPenColor(2,2,2);
 }
 
 void loop() {
-   PlotScene scene;
-   scene.start();
+  if (ps2c.keyboard()->isVKDown(fabgl::VK_RETURN)) {
+    canvas.clear();
+    written = false;
+  } else if (!written) {
+    canvas.clear();
+    canvas.drawText(100,80,"Hit ENTER when ready...");
+    written = true;
+  }
 }
