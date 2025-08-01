053f97540531e803560186910e254a8457fd8942
Andrew C. Young <andrew@vaelen.org>
2025-08-01 11:02:47 +0900

    Added a 256 color version and changed the color pallete to match the Mac color pallete.

--- unix/xmandelbrot.c
+++ unix/xmandelbrot.c
@@ -13,10 +13,18 @@
 #include <X11/Xutil.h>
 #include <sys/types.h>
 #include <sys/times.h>
-
-#define MAX_ITERATIONS 16
+#include <unistd.h>
 
 char *title = "xmandelbrot";
+
+struct color {
+  unsigned char r, g, b;
+};
+
+struct color colors_1bit[2];
+struct color colors_2bit[4];
+struct color colors_4bit[16];
+struct color colors_8bit[256];
 
 void get_window_size(display, window, height, width)
      Display *display;
@@ -30,16 +38,21 @@ void get_window_size(display, window, height, width)
   XGetGeometry(display, window, &root, &x, &y, width, height, &border, &depth);
 }
 
-unsigned long lookup_color(display, colormap, color_name, color)
+unsigned long lookup_color(display, colormap, color)
      Display *display;
      Colormap colormap;
-     char *color_name;
-     XColor *color;
+     struct color color;
 {
-  XColor exact, screen;
-  if (!XAllocNamedColor(display, colormap, color_name, &exact, &screen))
+  XColor screen;
+
+  /* Convert our 8-bit color space into a 16-bit color space */
+  screen.red = (color.r << 8) | color.r;
+  screen.green = (color.g << 8) | color.g;
+  screen.blue = (color.b << 8) | color.b;
+
+  if (!XAllocColor(display, colormap, &screen))
   {
-    printf("Color Not Found: %s\n", color_name);
+    printf("Color Not Found: rgb:%02x/%02x/%02x\n", color.r, color.g, color.b);
     return 0;
   }
   return screen.pixel;
@@ -75,7 +88,7 @@ void calculate(display, window, gc, palette, palette_size)
 
   for (row = 0; row < lines; row++)
   {
-    //    printf("Row: %u\n", row);
+    //    printf("Row: %u\n", row);q
     y0 = (row * 2.1 / height) - 1;
     for (col = 0; col < cols; col++)
     {
@@ -83,7 +96,7 @@ void calculate(display, window, gc, palette, palette_size)
       x = 0.0;
       y = 0.0;
       iteration = 0;
-      while ( ((x*x) + (y*y) <= 4) && (iteration < MAX_ITERATIONS))
+      while ( ((x*x) + (y*y) <= 4) && (iteration < palette_size) )
       {
         xtemp = (x*x) - (y*y) + x0;
         y = (2*x*y) + y0;
@@ -104,10 +117,129 @@ void calculate(display, window, gc, palette, palette_size)
 
 }
 
+struct color color(r, g, b)
+  unsigned char r, g, b;
+{
+  struct color c;
+  c.r = r;
+  c.g = g;
+  c.b = b;
+  return c;
+}
+
+void initialize_colors()
+{
+  int steps[16];
+  int idx, i, j, v, r, g, b;
+
+  /* Macintosh System 7 1-bit palette (Black, White) */
+  colors_1bit[0] = color(0x00, 0x00, 0x00);
+  colors_1bit[1] = color(0xff, 0xff, 0xff);
+
+  /* Macintosh System 7 2-bit palette */
+  colors_2bit[0] = color(0x00, 0x00, 0x00);
+  colors_2bit[1] = color(0x44, 0x44, 0x44);
+  colors_2bit[2] = color(0xbb, 0xbb, 0xbb);
+  colors_2bit[3] = color(0xff, 0xff, 0xff);
+
+  /* Macintosh System 7 4-bit palette (16 colors, XLib rgb format) */
+  colors_4bit[0]  = color(0x00, 0x00, 0x00);
+  colors_4bit[1]  = color(0x88, 0x00, 0x00);
+  colors_4bit[2]  = color(0x00, 0x88, 0x00);
+  colors_4bit[3]  = color(0x00, 0x00, 0x88);
+  colors_4bit[4]  = color(0x88, 0x88, 0x00);
+  colors_4bit[5]  = color(0x00, 0x88, 0x88);
+  colors_4bit[6]  = color(0x88, 0x00, 0x88);
+  colors_4bit[7]  = color(0x44, 0x44, 0x44);
+  colors_4bit[8]  = color(0xbb, 0xbb, 0xbb);
+  colors_4bit[9]  = color(0xff, 0x88, 0x88);
+  colors_4bit[10] = color(0x88, 0xff, 0x88);
+  colors_4bit[11] = color(0x88, 0x88, 0xff);
+  colors_4bit[12] = color(0xff, 0xff, 0x88);
+  colors_4bit[13] = color(0x88, 0xff, 0xff);
+  colors_4bit[14] = color(0xff, 0x88, 0xff);
+  colors_4bit[15] = color(0xff, 0xff, 0xff);
+
+  /* Macintosh 8-bit system palette (from lospec.com, 256 colors) */
+  idx = 0;
+  
+  steps[0] = 0x00;
+  steps[1] = 0x0b;
+  steps[2] = 0x22;
+  steps[3] = 0x44;
+  steps[4] = 0x55;
+  steps[5] = 0x77;
+  steps[6] = 0x88;
+  steps[7] = 0xaa;
+  steps[8] = 0xbb;
+  steps[9] = 0xdd;
+  steps[10] = 0xee;
+  steps[11] = 0x33;
+  steps[12] = 0x66;
+  steps[13] = 0x99;
+  steps[14] = 0xcc;
+  steps[15] = 0xff;
+
+  /* grayscale */
+  for (i = 0; i < 11; i++) 
+  {
+    v = steps[i];
+    colors_8bit[idx++] = color(v, v, v);
+  }
+
+  /* 10 blue, 10 green, 10 red */
+  for (j = 0; j < 3; j++)
+  {
+    for (i = 1; i < 11; i++) 
+    {
+      v = steps[i];
+      switch (j)
+      {
+        case 0: colors_8bit[idx++] = color(0x00, 0x00, v); break; /* blue */
+        case 1: colors_8bit[idx++] = color(0x00, v, 0x00); break; /* green */
+        case 2: colors_8bit[idx++] = color(v, 0x00, 0x00); break; /* red */
+      }
+    }
+  }
+
+  /* the rest */
+  for (r = 10; r < 16; r++)
+  {
+    for (g = 10; g < 16; g++)
+    {
+      for (b = 10; b < 16; b++)
+      {
+        if (r == 10 && g == 10 && b == 10) continue; /* skip black */
+        colors_8bit[idx++] = color(steps[r == 10 ? 0 : r], steps[g == 10 ? 0 : g], steps[b == 10 ? 0 : b]);
+      }
+    }
+  }
+
+  printf("Initialized %u colors.\n", idx);
+
+}
+
+void create_palette(display, colormap, palette, colors, size)
+     Display *display;
+     Colormap colormap;
+     unsigned long *palette;
+     struct color *colors;
+     size_t size;
+{
+  size_t i;
+  for (i = 0; i < size; i++)
+  {
+    palette[i] = lookup_color(display, colormap, colors[i]);
+  }
+}
+
 int main(argc,argv)
   int argc;
   char **argv;
 {
+
+  /* X11 variables */
+
   Display *display;
   Window  window;
  
@@ -123,40 +255,52 @@ int main(argc,argv)
   int i;
   char text[10];
   int done;
-
+  unsigned int width, height, lastWidth, lastHeight;
+  struct color *colors = colors_8bit;
   Colormap colormap;
   
-  unsigned long palette[16];
-  char *color_names[16];
-  color_names[0] = "Black";
-  color_names[1] = "DarkBlue";
-  color_names[2] = "DarkGreen";
-  color_names[3] = "DarkCyan";
-  color_names[4] = "DarkRed";
-  color_names[5] = "DarkMagenta";
-  color_names[6] = "Brown";
-  color_names[7] = "LightGray";
-  color_names[8] = "Gray";
-  color_names[9] = "Blue";
-  color_names[10] = "Green";
-  color_names[11] = "Cyan";
-  color_names[12] = "Red";
-  color_names[13] = "Magenta";
-  color_names[14] = "Yellow";
-  color_names[15] = "White";
+  unsigned long palette[256];
+
+  int max_iterations = 256;
+  if (argc > 1)
+  {
+    max_iterations = atoi(argv[1]);
+    if (max_iterations < 1 || max_iterations > 256)
+    {
+      fprintf(stderr, "Usage: %s [max_iterations (1-256)]\n", argv[0]);
+      exit(1);
+    }
+  }
+  initialize_colors();
 
   /* setup display/screen */
   display = XOpenDisplay("");
-  
+  if (!display) {
+    fprintf(stderr, "Error: Unable to open display\n");
+    exit(1);
+  }
+
   screen = DefaultScreen(display);
 
   colormap = DefaultColormap(display, screen);
 
-  for (i = 0; i < 16; i++)
+  if (max_iterations < 1 || max_iterations > 256)
   {
-    palette[i] = lookup_color(display, colormap, color_names[i]);
+    fprintf(stderr, "Error: max_iterations must be between 1 and 256\n");
+    exit(1);
   }
-  
+
+  if (max_iterations <= 2)
+    colors = colors_1bit;
+  else if (max_iterations <= 4)
+    colors = colors_2bit;
+  else if (max_iterations <= 16)
+    colors = colors_4bit;
+  else
+    colors = colors_8bit;
+
+  create_palette(display, colormap, palette, colors, max_iterations);
+
   /* drawing contexts for an window */
   bg = BlackPixel(display, screen);
   fg = WhitePixel(display, screen);
@@ -197,9 +341,17 @@ int main(argc,argv)
  
     switch(event.type){
     case Expose:
-      /* Window was showed. */
-      if(event.xexpose.count==0)
-	calculate(display, window, gc, palette, 16);
+      /* Window was shown. */
+      if(event.xexpose.count == 0)
+      {
+        get_window_size(display, window, &height, &width);
+        if (width != lastWidth || height != lastHeight)
+        {
+          calculate(display, window, gc, palette, max_iterations);
+        }
+        lastWidth = width;
+        lastHeight = height;
+      }
       break;
     case MappingNotify:
       /* Modifier key was up/down. */
